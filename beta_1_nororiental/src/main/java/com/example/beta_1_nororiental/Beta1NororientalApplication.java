0cd16e7b2259289dc26e451992d9223548ce7636
Bry <blyansviten@gmail.com>
2026-09-18 06:23:24 -0500

    feat se crea proyecto

---
+++ beta_1_nororiental/src/main/java/com/example/beta_1_nororiental/Beta1NororientalApplication.java
@@ -0,0 +1,13 @@
+package com.example.beta_1_nororiental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Beta1NororientalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Beta1NororientalApplication.class, args);
+	}
+
+}
